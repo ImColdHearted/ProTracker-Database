5193e5f6bd8eb72620d6661390921ea87d5b8868
ImColdHearted <135068207+ImColdHearted@users.noreply.github.com>
2026-08-20 18:36:58 -0700

    Add files via upload

---
+++ Converters/ColorToBrushConverter.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+using Avalonia.Data.Converters;
+using Avalonia.Media;
+
+namespace Foot_Tracker.Converters;
+
+public sealed class ColorToBrushConverter : IValueConverter
+{
+    public static readonly ColorToBrushConverter Instance = new();
+
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return value is Color color ? new SolidColorBrush(color) : null;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return value is SolidColorBrush brush ? brush.Color : null;
+    }
+}
